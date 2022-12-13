5546f2e09ab832a2e54ba17af47b32ae10106037
brewdiHQ <stephan.sattel@icloud.com>
2022-12-13 16:57:42 +0100

    Update visitor_guide_app.js
    add bind for console.log

--- 13 MD21VG/visitor_guide_app.js
+++ 13 MD21VG/visitor_guide_app.js
@@ -2,6 +2,10 @@ import { Objekt21, machwas } from './modules/Objekt21.js';
 import { logger21 } from './modules/logger.js';
 //import('./modules/Object21.js');
 
+// Test Stephan
+const C = console.log.bind(console);
+
+
 //Beispiel für den import einer Funktion
 machwas()
 
